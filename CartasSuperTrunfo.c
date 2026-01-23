33f07ffb37c474484026d438383e7743a87619bb
Cosmic <168202465+SirCosmic@users.noreply.github.com>
2026-01-23 10:43:24 +0000

    Feito a primeira parte do código, informando ao usuario oque será feito no primeiro momento.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,11 +5,10 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
 
-  // Área para entrada de dados
-
-  // Área para exibição dos dados da cidade
+  // Tutorial passado ao usuário, contendo as intruções do que irá acontecer.
+  printf("Bem vindo ao jogo de cartas Super Trunfo\n");
+  printf("Será pedido que você adicione duas cartas, inserindo informações: \n 1: Estado; uma letra de 'A' a 'H'. \n 2: Código da carta; Um número de 01 a 04. \n 3: O nome da cidade. \n 4: População; O número de habitantes. \n 5: Área; A área da cidade em quilômetros quadrados. \n 6: PIB; O Produto Interno Bruto da cidade. \n 7: Número de pontos turísticos; A quantidade de pontos turísticos na cidade \n ");
 
 return 0;
 } 
