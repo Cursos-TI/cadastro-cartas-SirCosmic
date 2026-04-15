6344396b8da763b3e849723310cc6f2cdcdb0c57
SirCosmic <williamkaspian@gmail.com>
2026-04-15 07:21:24 -0300

    Impressão de mensagens e scans para variáveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,11 +7,35 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
     char estado, codigo[10], nomeDaCidade[50];
-    int população, pontosTuristicos;
+    int carta, população, pontosTuristicos;
     float area, PIB;
   // Área para entrada de dados
-
+    printf("Digite o número da carta: ");
+    scanf("%d", &carta);
+    printf("Digite o estado da cidade: ");
+    scanf("%s", &estado);
+    printf("Digite o código da cidade: ");
+    scanf("%s", codigo);
+    printf("Digite o nome da cidade: ");
+    scanf("%s", nomeDaCidade);
+    printf("Digite a população da cidade: ");
+    scanf("%d", &população);
+    printf("Digite a área da cidade (em km²): ");
+    scanf("%f", &area);
+    printf("Digite o PIB da cidade (em bilhões): ");
+    scanf("%f", &PIB);
+    printf("Digite o número de pontos turísticos da cidade: ");
+    scanf("%d", &pontosTuristicos);
   // Área para exibição dos dados da cidade
+    printf("\n--- Carta %d ---\n", carta);
+    printf("\n--- Dados da Cidade ---\n");
+    printf("Estado: %c\n", estado);
+    printf("Código: %s\n", codigo);
+    printf("Nome: %s\n", nomeDaCidade);
+    printf("População: %d\n", população);
+    printf("Área (km²): %.2f\n", area);
+    printf("PIB (bilhões): %.2f\n", PIB);
+    printf("Pontos Turísticos: %d\n", pontosTuristicos);
 
 return 0;
 } 
